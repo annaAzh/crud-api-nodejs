885cbcc2a4b0b7c67ee6ca61c7a8656bfafbe6ab
annaAzh <forarre@yandex.ru>
2026-03-21 21:04:18 +0300

    feat(src/module): add product-routes

---
+++ src/module/product.routes.ts
@@ -0,0 +1,24 @@
+import {
+  addProduct,
+  deleteProduct,
+  getOneProduct,
+  getProducts,
+  updateProduct
+} from './product.controller.ts'
+import type { FastifyInstance } from 'fastify'
+import {
+  createProductSchema,
+  deleteProductSchema,
+  getProductSchema,
+  updateProductSchema
+} from '../schemas/index.ts'
+
+const productRoutes = async (fastify: FastifyInstance) => {
+  fastify.get('/products', getProducts)
+  fastify.get('/products/:id', { schema: getProductSchema }, getOneProduct)
+  fastify.post('/products', { schema: createProductSchema }, addProduct)
+  fastify.put('/products/:id', { schema: updateProductSchema }, updateProduct)
+  fastify.delete('/products/:id', { schema: deleteProductSchema }, deleteProduct)
+}
+
+export default productRoutes
